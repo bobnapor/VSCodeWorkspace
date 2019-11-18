b8063eb37fa62c5930a0ba1a9fd0f743ac97a194
Bobby Napor <bobnapor@gmail.com>
2019-11-18 17:23:00 -0500

    adding pull of def stats

--- nflpicks.py
+++ nflpicks.py
@@ -36,7 +36,7 @@ column_names = []
 
 for year in range(2009, 2020):
     local_off_url = file_dir + local_off_template.replace('yyyy', str(year))
-    #local_def_url = file_dir + local_def_template.replace('yyyy', str(year))
+    local_def_url = file_dir + local_def_template.replace('yyyy', str(year))
     #local_games_url = file_dir + local_games_template.replace('yyyy', str(year))
 
     single_year_stats = dict()
@@ -50,7 +50,6 @@ for year in range(2009, 2020):
         else:
             off_stats_table = off_stats_tables[0]
             off_stats_rows = off_stats_table.find_all('tbody')[0].find_all('tr')
-
             for off_stats_row in off_stats_rows:
                 single_team_stats = dict()
                 for off_stat_column in off_stats_row.find_all('td'):
@@ -60,8 +59,23 @@ for year in range(2009, 2020):
                 single_year_stats[team] = single_team_stats
                 print('Extracted data for the ' + str(year) + ' ' + team)
 
+    #TODO: single_year_stats[team][<def stat column>] = <stat>
+    local_def_file = open(local_def_url)
+    local_def_soup = BeautifulSoup(local_def_file.read(), 'html.parser')
+    def_stats_table = local_def_soup.find_all('table', id='team_stats')[0]
+    for def_stats_row in def_stats_table.find_all('tbody')[0].find_all('tr'):
+        single_team_stats_def = dict()
+        for def_stat_column in def_stats_row.find_all('td'):
+            column_name = 'def_' + def_stat_column['data-stat']
+            single_team_stats_def[column_name] = def_stat_column.text
+        team = single_team_stats_def['def_team']
+        for def_stat_key in single_team_stats_def: #right way to loop through keys?
+            single_year_stats[team][def_stat_key] = single_team_stats_def[def_stat_key]
+
     year_stats[year] = single_year_stats
 
-print(year_stats)
 
+
+
+print(year_stats)
 print('Completed!')
