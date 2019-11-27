172eaa9564a724a1c9f09f8072726f34d2352179
Bobby Napor <bobnapor@gmail.com>
2019-11-27 13:00:16 -0500

    example plot code

---
+++ linear_regression_improvement.py
@@ -0,0 +1,19 @@
+import pandas as pd  
+import numpy as np  
+import matplotlib.pyplot as plt  
+import seaborn as seabornInstance 
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+
+# x-axis values 
+x = [5, 2, 9, 4, 7]
+
+# Y-axis values 
+y = [10, 5, 8, 4, 2]
+
+# Function to plot 
+plt.plot(x, y)
+
+# function to show the plot 
+plt.show()
